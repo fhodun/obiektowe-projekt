dde2dc83371987d5c27f1a28129506f134a69317
Filip Hodun <filiphodun@icloud.com>
2026-02-10 20:01:45 +0100

    initial commit

---
+++ obiektowe-projekt/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace obiektowe_projekt;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
